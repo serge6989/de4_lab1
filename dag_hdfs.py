a105d9d599c40e84e8f58dc05746b2382d1334e9
serge6989 <48008667+serge6989@users.noreply.github.com>
2019-03-15 20:53:03 +0300

    Create dag_hdfs.py

---
+++ dag_hdfs.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import elasticsearch
+import csv
+import unicodedata
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+dag = DAG('Route_to_HDFS', description='no_desc',
+          schedule_interval='* 1 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+b1 = BashOperator(task_id='put_data_to_hdfs',
+                  bash_command='hdfs dfs -put /etc/airflow/airflow_home/temp/2019-03-15_elastic.file /opt/lab1',
+                  dag=dag)
